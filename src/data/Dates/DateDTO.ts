514133db61c0cc91136e0298c6c246403c280818
Eduardo Franco <franc0edu24@gmail.com>
2024-03-16 16:22:57 -0300

    build: :construction: defining project Data Transfer Object
    defining project Data Transfer Object

---
+++ src/data/Dates/DateDTO.ts
@@ -0,0 +1,6 @@
+import { SnackDTO } from "@data/Snack/SnackDTO";
+
+export interface DateDTO{
+  date: string;
+  snacks: SnackDTO[];
+}
---
+++ src/data/Snack/SnackDTO.ts
@@ -0,0 +1,8 @@
+export interface SnackDTO{
+  id: string;
+  name: string;
+  description: string;
+  date: string;
+  hour: string;
+  onDiet: string;
+}
