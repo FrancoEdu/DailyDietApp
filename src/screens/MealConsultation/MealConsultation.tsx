8974652fdf7c6630374dfba00dbfd90ad7def1a1
Eduardo Franco <franc0edu24@gmail.com>
2024-03-21 20:55:39 -0300

    feat: :seedling: Screens finalizadas
    Screens finished
    
    Screens finished

--- src/screens/MealConsultation/MealConsultation.tsx
+++ src/screens/MealConsultation/MealConsultation.tsx
@@ -1,7 +1,7 @@
 import { BackButton } from "@components/BackButton/BackButton";
-import { Container, Header } from "./StyleMealConsultation";
-
-
+import { Bottom, Container, Description, Header, HourDate, HourDateTitle, Snack } from "./StyleMealConsultation";
+import { View } from "react-native";
+import { Button } from "@components/Button/Button";
 
 export function MealConsultantion(){
   return(
@@ -9,7 +9,22 @@ export function MealConsultantion(){
       <Header onDiet={false}>
         <BackButton textTitle="Refeição" backgroundType="TERTIARY" type="TERTIARY"/>
       </Header>
-      
+      <Bottom>
+        <Snack>X-TUDO</Snack>
+        <Description>
+          Xis completo da lancheria do bairro
+        </Description>
+        <HourDateTitle>
+          Data e hora
+        </HourDateTitle>
+        <HourDate>
+          21/03/2024 às 20:00
+        </HourDate>
+        <View style={{flex: 1, justifyContent: 'flex-end', flexDirection: 'column', gap: 10, marginBottom: 150}}>
+          <Button type="PRIMARY" icon="edit" title="Editar refeição" />
+          <Button type="SECUNDARY" icon="restore-from-trash" title="Excluir refeição" />
+        </View>
+      </Bottom>
     </Container>
   )
 }
--- src/screens/MealConsultation/StyleMealConsultation.ts
+++ src/screens/MealConsultation/StyleMealConsultation.ts
@@ -18,3 +18,44 @@ export const Header = styled.View<Props>`
   align-items: center;
   padding: 24px;
 `;
+
+export const Bottom = styled.View`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 15%;
+  left: 0;
+  z-index: 2;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  border-radius: 20px 20px;
+  flex-direction: column;
+  background-color: ${({ theme}) => theme.COLORS.WHITE};
+  padding: 24px;
+  gap: 10px;
+`;
+
+export const Snack = styled.Text`
+  margin-top: 40px;
+  font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  color: ${({ theme }) => theme.COLORS.GRAY_700};
+`;
+
+export const Description = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  color: ${({ theme }) => theme.COLORS.GRAY_700};
+`;
+
+export const HourDateTitle = styled.Text`
+  margin-top: 20px;
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  color: ${({ theme }) => theme.COLORS.GRAY_700};
+`;
+
+export const HourDate = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  color: ${({ theme }) => theme.COLORS.GRAY_700};
+`;
